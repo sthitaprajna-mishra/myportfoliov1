6b0bdd797b714947ae1295ec32881ab7b182d875
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-29 09:16:51 +0530

    added navbar sections

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ import LunchDiningIcon from "@mui/icons-material/LunchDining";
 
 const Home = () => {
   return (
-    <div className="flex flex-col ml-20 justify-evenly border-1 border-green-500 h-screen">
+    <div className="w-fit flex flex-col ml-20 justify-evenly border-1 border-green-500 h-screen">
       <div className="md:hidden">
         <LunchDiningIcon className="text-orange" style={{ fontSize: "3rem" }} />
       </div>
 
-      <div>
+      <div className="border-1 w-fit border-black">
         <div className="ml-1 text-lg font-bold tracking-wider">Hi, I am</div>
         <div className="text-red font-bold text-8xl">Sthitaprajna Mishra</div>
         <div className="text-5xl font-semibold">
@@ -17,14 +17,14 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="w-2/3 text-2xl">
+      <div className="w-2/3 border-1 border-black text-2xl">
         Welcome to my digital portfolio! I possess a flair for coding,
         creativity, and a profound love for{" "}
         <span className="text-red font-semibold">full stack engineering</span>.
         Let's connect, collaborate, and create. Together, we'll shape the
         future, one line of code at a time.
       </div>
-      <div>
+      <div className="w-2/3 border-1 border-black">
         <div>
           Reach out to me at <span className="underline">LinkedIn</span>,{" "}
           <span className="underline">GitHub</span>, and{" "}
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -4,8 +4,16 @@ import Home from "./Home";
 
 const Wrapper = () => {
   return (
-    <div className="bg-lightWhite text-darkBlue font-leagueSpartan max-w-[1460px] border-1 border-blue-500 flex justify-center mx-auto">
-      <Home />
+    <div className="bg-lightWhite text-darkBlue font-leagueSpartan max-w-[1460px] border-1 border-blue-500 grid grid-cols-12 mx-auto">
+      <div className="col-span-10">
+        <Home />
+      </div>
+      <div className="col-span-2 my-auto text-xl space-y-6">
+        <div>Introduction</div>
+        <div>Experience</div>
+        <div>Skills</div>
+        <div>Projects</div>
+      </div>
     </div>
   );
 };
