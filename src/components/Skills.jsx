068f22d870d9b0a52e99e1fb2864942601cd929b
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-29 21:08:32 +0530

    staggered animation skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { useInView } from "framer-motion";
+import { motion, useInView } from "framer-motion";
 
 import htmlIcon from "../assets/html5.svg";
 import cssIcon from "../assets/css3.svg";
@@ -7,7 +7,6 @@ import jsIcon from "../assets/javascript.svg";
 import sassIcon from "../assets/sass.svg";
 import tailwindcssIcon from "../assets/tailwindcss.svg";
 import reactIcon from "../assets/react.svg";
-
 import javaIcon from "../assets/java.svg";
 import csharpIcon from "../assets/csharp.svg";
 import dotnetcoreIcon from "../assets/dot-net-core.svg";
@@ -18,15 +17,29 @@ const skillsData = [
   { icon: htmlIcon, name: "HTML" },
   { icon: cssIcon, name: "CSS" },
   { icon: jsIcon, name: "JavaScript" },
-  { icon: sassIcon, name: "sass/scss" },
-  { icon: tailwindcssIcon, name: "tailwind css" },
-  { icon: reactIcon, name: "react" },
-  { icon: javaIcon, name: "java" },
-  { icon: csharpIcon, name: "c#" },
-  { icon: dotnetcoreIcon, name: "web apis" },
-  { icon: mssqlIcon, name: "ms sql" },
-  { icon: githubIcon, name: "github" },
+  { icon: sassIcon, name: "SASS/SCSS" },
+  { icon: tailwindcssIcon, name: "Tailwind CSS" },
+  { icon: reactIcon, name: "React" },
+  { icon: javaIcon, name: "Java" },
+  { icon: csharpIcon, name: "C#" },
+  { icon: dotnetcoreIcon, name: "Web APIs" },
+  { icon: mssqlIcon, name: "MSSQL" },
+  { icon: githubIcon, name: "GitHub" },
 ];
+
+const fadeInAnimationVariations = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
 
 const Skills = ({ setIsSkillInView }) => {
   const ref = useRef(null);
@@ -41,23 +54,36 @@ const Skills = ({ setIsSkillInView }) => {
   }, [isInView]);
   return (
     <div className="w-fit flex flex-col ml-20 justify-evenly border-1 border-green-500 h-screen">
-      <div
+      <motion.div
+        initial={{ marginLeft: "-2000px", opacity: 0 }}
+        whileInView={{ marginLeft: "0", opacity: 1 }}
+        transition={{ duration: 0.75 }}
         ref={ref}
         className="text-5xl font-semibold text-darkBlue underline decoration-blue2"
       >
         Skills
-      </div>
+      </motion.div>
 
       <div className="text-xl w-5/6 text-gray1 flex flex-wrap gap-16">
         {skillsData.map((skill, index) => (
-          <div key={index} className="flex flex-col items-center gap-y-6">
+          <motion.div
+            variants={fadeInAnimationVariations}
+            initial="initial"
+            whileInView="animate"
+            viewport={{
+              once: true,
+            }}
+            custom={index}
+            key={index}
+            className="flex flex-col items-center gap-y-6"
+          >
             <div>
               <img className="h-20 w-20" src={skill.icon} alt={skill.name} />
             </div>
-            <div className="font-spaceGrotesk text-sm font-semibold uppercase">
+            <div className="font-spaceGrotesk text-sm font-bold">
               {skill.name}
             </div>
-          </div>
+          </motion.div>
         ))}
       </div>
     </div>
