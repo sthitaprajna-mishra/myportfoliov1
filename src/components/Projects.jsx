36f83322bb52f4c455a11ddeb6e8e86adc3cc204
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-30 23:32:48 +0530

    project section changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import LanguageIcon from "@mui/icons-material/Language";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 import dictionaryDesktopDark from "../assets/dictionary_desktop_dark.png";
 import dictionaryDesktopLight from "../assets/dictionary_desktop_light.png";
 
 const Projects = () => {
+  const [dictionaryProjectHover, setDictionaryProjectHover] = useState(false);
+
   return (
     <div
       id="projects"
@@ -12,14 +18,51 @@ const Projects = () => {
       <div className="text-5xl font-semibold text-darkBlue underline decoration-blue2">
         Projects
       </div>
-      <div className="flex gap-x-6">
-        <div className="flex-1">
-          <img src={dictionaryDesktopDark} alt="dictionaryDesktopDark" />
+      <div className="flex items-center gap-x-12">
+        <div className="flex-1 space-y-4">
+          <img
+            className="hover:cursor-pointer transition-all"
+            src={dictionaryDesktopDark}
+            alt="dictionaryDesktopDark"
+            onMouseEnter={() => setDictionaryProjectHover(true)}
+            onMouseLeave={() => setDictionaryProjectHover(false)}
+          />
+          <div
+            className={`${
+              dictionaryProjectHover ? "text-darkBlue" : "text-lightWhite"
+            } text-center text-gray1`}
+          >
+            Click to open expanded view
+          </div>
         </div>
-        <div className="flex-1 pr-8 flex flex-col gap-y-2">
+        <div className="flex-1 pr-8 flex flex-col gap-y-6">
           <h2 className="text-3xl font-semibold">Dictionary Web App</h2>
 
-          <div className="w-11/12 space-y-2">
+          <div className="flex text-md gap-x-8">
+            <div className="flex items-center gap-x-2">
+              <GitHubIcon size="small" />
+              <div className="flex items-center gap-x-1">
+                <a
+                  target="_blank"
+                  href="https://github.com/sthitaprajna-mishra/dictionary-webapp"
+                >
+                  GitHub
+                </a>
+                <OpenInNewIcon style={{ fontSize: "1rem" }} />
+              </div>
+            </div>
+            <div className="flex items-center gap-x-2">
+              <LanguageIcon />
+              <div className="flex items-center gap-x-1">
+                <a target="_blank" href="https://vocabsearch.netlify.app/">
+                  Live Site
+                </a>
+                <OpenInNewIcon style={{ fontSize: "1rem" }} />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-11/12 space-y-2 text-justify">
             <p>
               A web application that seamlessly integrates with the Dictionary
               API, providing users with the ability to effortlessly search for
@@ -35,29 +78,20 @@ const Projects = () => {
               scheme that corresponds to their computer settings, or it allows
               manual toggling for a personalized visual experience.
             </p>
-            <p>
-              This real-world dictionary web app combines functionality and
-              aesthetics, offering a seamless and intuitive experience for users
-              seeking word definitions and pronunciations.
-            </p>
           </div>
 
-          <div className="flex flex-col gap-y-2">
-            <div className="font-semibold">Tech Stack</div>
-            <div className="flex gap-x-6">
-              <div>React</div>
-              <div>Tailwind CSS</div>
-              <div>Material UI</div>
-              <div>Axios</div>
+          <div className="flex gap-x-4">
+            <div className="bg-blue2 px-4 pt-1 text-lightWhite rounded-full border border-blue2 transition-all hover:bg-lightWhite hover:border hover:text-darkBlue">
+              React
             </div>
-          </div>
-
-          <div className="flex gap-x-8">
-            <div>
-              <a href="#">GitHub</a>
+            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
+              Tailwind CSS
             </div>
-            <div>
-              <a href="#">Live Site</a>
+            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
+              Material UI
+            </div>
+            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
+              Axios
             </div>
           </div>
         </div>
