295b1cfcf3f8c6d47fc129e06ec948b7b99691c5
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-11-01 18:14:35 +0530

    added project item logic

---
+++ src/components/ProjectItem.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import LanguageIcon from "@mui/icons-material/Language";
+import GitHubIcon from "@mui/icons-material/GitHub";
+
+const ProjectItem = ({ project }) => {
+  const [dictionaryProjectHover, setDictionaryProjectHover] = useState(false);
+
+  return (
+    <div className="flex items-center gap-x-12">
+      <div className="flex-1 space-y-4">
+        <img
+          className="hover:cursor-pointer transition-all"
+          src={project.photos[0]}
+          alt="dictionaryDesktopDark"
+          onMouseEnter={() => setDictionaryProjectHover(true)}
+          onMouseLeave={() => setDictionaryProjectHover(false)}
+        />
+        <div
+          className={`${
+            dictionaryProjectHover ? "text-darkBlue" : "text-lightWhite"
+          } text-center text-gray1`}
+        >
+          Click to open expanded view
+        </div>
+      </div>
+      <div className="flex-1 pr-8 flex flex-col gap-y-6">
+        <h2 className="text-4xl bg-sampleBg2 bg-no-repeat bg-cover bg-clip-text text-transparent font-bold ">
+          {project.name}
+        </h2>
+        <div className="flex text-md gap-x-8">
+          <div className="flex items-center gap-x-2">
+            <GitHubIcon size="small" />
+            <div className="flex items-center gap-x-1 text-gray1">
+              <a
+                className="hover:text-red"
+                target="_blank"
+                href={project.codeLink}
+              >
+                Code
+              </a>
+              <OpenInNewIcon style={{ fontSize: "1rem" }} />
+            </div>
+          </div>
+          <div className="flex items-center gap-x-2">
+            <LanguageIcon className="text-blue1" />
+            <div className="flex items-center gap-x-1 text-gray1">
+              <a
+                className="hover:text-red"
+                target="_blank"
+                href={project.liveServerLink}
+              >
+                Live Site
+              </a>
+              <OpenInNewIcon style={{ fontSize: "1rem" }} />
+            </div>
+          </div>
+        </div>
+
+        {project.details()}
+
+        <div className="flex gap-x-4">
+          {project.techStack.map((tech) => {
+            return (
+              <div className="bg-blue2 px-4 pt-1 text-lightWhite rounded-full border border-blue2 transition-all hover:bg-lightWhite hover:border hover:text-darkBlue">
+                {tech}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectItem;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,49 @@
 import React, { useState } from "react";
 
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-import LanguageIcon from "@mui/icons-material/Language";
-import GitHubIcon from "@mui/icons-material/GitHub";
-
 import dictionaryDesktopDark from "../assets/dictionary_desktop_dark.png";
 import dictionaryDesktopLight from "../assets/dictionary_desktop_light.png";
 
-const Projects = () => {
-  const [dictionaryProjectHover, setDictionaryProjectHover] = useState(false);
+import ProjectItem from "./ProjectItem";
 
+const projectsInfo = [
+  {
+    id: 1,
+    name: "Dictionary Web App",
+    photos: [dictionaryDesktopDark],
+    codeLink: "https://github.com/sthitaprajna-mishra/dictionary-webapp",
+    liveServerLink: "https://vocabsearch.netlify.app/",
+    details: () => {
+      return (
+        <div className="w-11/12 space-y-2 text-justify">
+          <p>
+            A web application that seamlessly integrates with the Dictionary
+            API, providing users with the ability to effortlessly{" "}
+            <span className="text-blue2 font-bold">search for words</span> and
+            receive comprehensive word definitions and information .
+          </p>
+          <p>
+            In addition, the application{" "}
+            <span className="text-blue2 font-bold">
+              supports audio playback
+            </span>
+            , enabling users to listen to word pronunciations when available.
+          </p>
+          <p>
+            The application caters to users' preferences by seamlessly adapting
+            to their computer settings, automatically aligning with the color
+            scheme. Additionally, it provides the flexibility for users to
+            manually toggle the{" "}
+            <span className="text-blue2 font-bold">dark mode</span>, ensuring a
+            personalized interface.
+          </p>
+        </div>
+      );
+    },
+    techStack: ["React", "Tailwind CSS", "Material UI", "Axios"],
+  },
+];
+
+const Projects = () => {
   return (
     <div
       id="projects"
@@ -18,84 +52,9 @@ const Projects = () => {
       <div className="text-5xl font-semibold text-darkBlue underline decoration-blue2">
         Projects
       </div>
-      <div className="flex items-center gap-x-12">
-        <div className="flex-1 space-y-4">
-          <img
-            className="hover:cursor-pointer transition-all"
-            src={dictionaryDesktopDark}
-            alt="dictionaryDesktopDark"
-            onMouseEnter={() => setDictionaryProjectHover(true)}
-            onMouseLeave={() => setDictionaryProjectHover(false)}
-          />
-          <div
-            className={`${
-              dictionaryProjectHover ? "text-darkBlue" : "text-lightWhite"
-            } text-center text-gray1`}
-          >
-            Click to open expanded view
-          </div>
-        </div>
-        <div className="flex-1 pr-8 flex flex-col gap-y-6">
-          <h2 className="text-3xl font-semibold">Dictionary Web App</h2>
-
-          <div className="flex text-md gap-x-8">
-            <div className="flex items-center gap-x-2">
-              <GitHubIcon size="small" />
-              <div className="flex items-center gap-x-1">
-                <a
-                  target="_blank"
-                  href="https://github.com/sthitaprajna-mishra/dictionary-webapp"
-                >
-                  GitHub
-                </a>
-                <OpenInNewIcon style={{ fontSize: "1rem" }} />
-              </div>
-            </div>
-            <div className="flex items-center gap-x-2">
-              <LanguageIcon />
-              <div className="flex items-center gap-x-1">
-                <a target="_blank" href="https://vocabsearch.netlify.app/">
-                  Live Site
-                </a>
-                <OpenInNewIcon style={{ fontSize: "1rem" }} />
-              </div>
-            </div>
-          </div>
-
-          <div className="w-11/12 space-y-2 text-justify">
-            <p>
-              A web application that seamlessly integrates with the Dictionary
-              API, providing users with the ability to effortlessly search for
-              words and receive comprehensive word definitions and information.
-            </p>
-            <p>
-              In addition, the application supports audio playback, enabling
-              users to listen to word pronunciations when available.
-            </p>
-            <p>
-              The application has been designed with user experience in mind. It
-              accommodates users' preferences by automatically adopting a color
-              scheme that corresponds to their computer settings, or it allows
-              manual toggling for a personalized visual experience.
-            </p>
-          </div>
-
-          <div className="flex gap-x-4">
-            <div className="bg-blue2 px-4 pt-1 text-lightWhite rounded-full border border-blue2 transition-all hover:bg-lightWhite hover:border hover:text-darkBlue">
-              React
-            </div>
-            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
-              Tailwind CSS
-            </div>
-            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
-              Material UI
-            </div>
-            <div className="bg-blue2 py-1 px-4 text-lightWhite rounded-full">
-              Axios
-            </div>
-          </div>
-        </div>
-      </div>
+      {projectsInfo.map((projectItem) => {
+        return <ProjectItem key={projectItem.id} project={projectItem} />;
+      })}
     </div>
   );
 };
